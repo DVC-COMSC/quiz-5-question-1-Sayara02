2e2ad91ecb5968b3bb596ae8d8cbfbd37795a204
Sayara02 <azizovna02@gmail.com>
2024-03-08 19:28:30 -0800

    2 run

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ int main()
    int rows;
    cout << "Enter the number of rows: ";
    cin >> rows;
+   for (int i = 0; i < rows; ++i) {
+      char letter = 'A';
+      
+   }
    /* Complete your code */
   
 }
